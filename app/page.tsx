a4e9b6cc2f0ef78d75236165dbbcbd601882f52f
tom783554-ui <tom783554@gmail.com>
2026-01-18 11:34:23 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import Viewer from "./viewer/Viewer";
+'use client';
 
-export default function Page() {
-  return <Viewer />;
+import dynamic from 'next/dynamic';
+
+const Viewer = dynamic(() => import('./viewer/Viewer'), {
+  ssr: false,
+});
+
+export default function HomePage() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Viewer />
+    </div>
+  );
 }
